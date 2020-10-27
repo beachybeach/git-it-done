28230884fca95acf278125cf0295650562b62eec
Riley <rsbeach97@gmail.com>
2020-10-27 12:50:13 -0700

    finished module 5

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -2,7 +2,7 @@ var userFormEl = document.querySelector("#user-form");
 var nameInputEl = document.querySelector("#username");
 var repoContainerEl = document.querySelector("#repos-container");
 var repoSearchTerm = document.querySelector("#repo-search-term");
-
+var languageButtonsEl = document.querySelector("div#language-buttons");
 
 
 var getUserRepos = function(user) {
@@ -103,4 +103,29 @@ if (username) {
       }
   };
 
+  var getFeaturedRepos = function(language) {
+    var apiUrl = "https://api.github.com/search/repositories?q=" + language + "+is:featured&sort=help-wanted-issues";
+
+    fetch(apiUrl).then(function(response) {
+        if (response.ok) {
+            response.json().then(function(data) {
+            displayRepos(data.items, language);
+            });
+        } else {
+            alert("Error: " + response.statusText);
+        }
+    });
+  };
+
   userFormEl.addEventListener("submit", formSubmitHandler);
+  var buttonClickHandler = function(event) {
+      var language = event.target.getAttribute("data-language");
+      if (language) {
+          getFeaturedRepos(language);
+
+          //clear old content
+          repoContainerEl.textContent = "";
+      }
+  }
+
+  languageButtonsEl.addEventListener("click", buttonClickHandler);
