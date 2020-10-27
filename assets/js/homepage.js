9a7795d7e4a8e83e6b20b9decede34a3d77253c2
Riley <rsbeach97@gmail.com>
2020-10-27 09:17:40 -0700

    module 6.2 complete

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -10,10 +10,19 @@ var getUserRepos = function(user) {
     var apiUrl = "https://api.github.com/users/" + user + "/repos";
 
     //make a request to the url
-   fetch(apiUrl).then(function(response) {
+   fetch(apiUrl)
+   .then(function(response) {
+       //request was successful
+       if (response.ok) {
         response.json().then(function(data) {
             displayRepos(data, user);
         });
+    } else {
+        alert("Error: " + response.statusText);
+    }
+    })
+    .catch(function(error) {
+        alert("Unable to connect to Github");
     });
 };
 
@@ -40,6 +49,11 @@ if (username) {
  
 
   var displayRepos = function(repos, searchTerm) {
+      //check if api returned any repos
+      if (repos.length === 0) {
+          repoContainerEl.textContent = "No repositories found.";
+          return;
+      }
       //clear old content
       repoContainerEl.textContent = "";
       repoSearchTerm.textContent = searchTerm;
